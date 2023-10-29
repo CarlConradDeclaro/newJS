20eaa37f05a7c81646fe09de08d9e20fce498074
Mark <nunezchristian77@gmail.com>
2023-10-29 19:34:09 +0800

    added a navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,21 +1,43 @@
 import { BiMenu } from 'react-icons/bi';
+import {AiOutlineSearch} from 'react-icons/ai'
+import {FiSearch} from 'react-icons/fi'
+import {BiArrowBack} from 'react-icons/bi'
+import { useState } from 'react';
 
 const Navbar = () => {
+
+  const [showSearch, setshowSearch] = useState(false);
 
   const toggle = () => {
     const side = document.querySelector('.sidebar')
     const backdrop = document.querySelector('.backdrop')
     side.classList.toggle('show')
     backdrop.classList.toggle('darken')
-
-    console.log('click')
   }  
-  
 
   return (
     <header className="navbar container">
-         <BiMenu className="toggle-icon" onClick={toggle} />
-        <h1 className="logo">Navbar</h1>
+
+      {showSearch && 
+        <div className="clicked-search">
+          <div className="back-container" onClick={() => setshowSearch(false)}>
+            <BiArrowBack className='back'></BiArrowBack>
+          </div>
+          <form action="">
+            <input type="text" placeholder='Search'/>
+          </form>
+        </div>
+      }
+
+        <BiMenu className="toggle-icon" onClick={toggle} />
+        <h1 className="logo">LOGO</h1>
+        <form className="input-section">
+          <input type="text" placeholder='Search'/>
+          <div className="search-container" onClick={() => setshowSearch(true)}>
+            <FiSearch className='search-icon' ></FiSearch>
+          </div>
+        </form>
+
     </header>
   )
 }
